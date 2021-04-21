ae5bb69cf5c1217b861c8c2402f5636ccb5eddea
Graziele Dantas <elentary@gmail.com>
2021-04-20 21:21:15 -0300

    done all

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,104 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
+/*for (let i = 0; i < mashPotatoes.length; i++) {
   //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-}
+  console.log(mashPotatoes[i]);
+}*/
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
-  addFood(steak[1], '#steak', () => {
-
-  })
+const steakCooked = new Promise((resolve, reject) => {
+  addFood(steak[0], "#steak", () => {
+    addFood(steak[1], "#steak", () => {
+      addFood(steak[2], "#steak", () => {
+        addFood(steak[3], "#steak", () => {
+          addFood(steak[4], "#steak", () => {
+            addFood(steak[5], "#steak", () => {
+              addFood(steak[6], "#steak", () => {
+                addFood(steak[7], "#steak", () => {
+                  //console.log('Steak cooked');
+                  document.getElementById("table").innerHTML +=
+                    '<img src="public/images/steak.jpg">';
+                  resolve();
+                  console.log(steakCooked);
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
 });
-
-
 
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+const potatoesCooked = new Promise((resolve, reject) => {
+  addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
+    // ... your code here
+    addFood(mashPotatoes[1], "#mashPotatoes").then(() => {
+      addFood(mashPotatoes[2], "#mashPotatoes").then(() => {
+        addFood(mashPotatoes[3], "#mashPotatoes").then(() => {
+          addFood(mashPotatoes[4], "#mashPotatoes").then(() => {
+            document.getElementById("table").innerHTML +=
+              '<img src="public/images/mashPotatoes.jpg">';
+            resolve();
+            console.log(potatoesCooked);
+          });
+        });
+      });
+    });
+  });
+}).catch((err) => {
+  console.error(err);
 });
+
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
+// const sproutsCooked = new Promise ((resolve, reject) => {
+//   async function makeFood(steps) {
+//     for (let i = 0; i < steps; i++) {
+//       await addFood(brusselSprouts[i], "#brusselSprouts").then((result) => {
+//         document.getElementById('table').innerHTML+='<img src=\"public/images/brusselSprouts.jpg\">';
+//         resolve();
+//         console.log(sproutsCooked);
+//       });
+//     }
+
+//   }
+//   makeFood(9);
+// });
+const sproutsCooked = new Promise((resolve, reject) => {
+  async function makeFood() {
+    await addFood(brusselSprouts[1], "#brusselSprouts");
+    await addFood(brusselSprouts[2], "#brusselSprouts");
+    await addFood(brusselSprouts[3], "#brusselSprouts");
+    await addFood(brusselSprouts[4], "#brusselSprouts");
+    await addFood(brusselSprouts[5], "#brusselSprouts");
+    await addFood(brusselSprouts[6], "#brusselSprouts");
+    await addFood(brusselSprouts[7], "#brusselSprouts");
+    await addFood(brusselSprouts[8], "#brusselSprouts").then((result) => {
+      document.getElementById("table").innerHTML +=
+        '<img src="public/images/brusselSprouts.jpg">';
+      resolve();
+      console.log(sproutsCooked);
+    });
   }
-  makeFood(eachStep);
+  makeFood();
+});
+
+
+// //BONUS 2
+// Using promise.all when all dinner items (Steak, Mash Potatoes, and Brussels Sprouts) are all done and added to the table, alert Dinner is served!
+
+Promise.all([steakCooked, potatoesCooked, sproutsCooked]).then((resolve) => {
+  document.querySelector("body").innerHTML += `<button id="btn">Dinner is served!</button>`;
+  document.getElementById("btn").addEventListener("click", audio);
+});
+
+const dinnerSound = new Audio();
+dinnerSound.src = 'public/media/dinnerIsServed.mp3';
+
+function audio () {
+  dinnerSound.play();
+}
